64c53b5a02da58aeaf1af0881b0dbdfa88755585
suwa <baru.web.production@gmail.com>
2021-07-12 08:43:20 +0900

    タスクのtitleカラムにnull: falseを追加する

--- db/migrate/20210708113526_create_tasks.rb
+++ db/migrate/20210708113526_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.text :discription
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
