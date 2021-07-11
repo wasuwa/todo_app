55bbdc063322565feb8e512b7e7dc5c62f0dbf5e
suwa <baru.web.production@gmail.com>
2021-07-11 16:19:38 +0900

    taskのcreateアクションを作成する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,25 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
+    @tasks = Task.all
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_url
+    else
+      render root_path
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title, :discription)
+    end
 end
