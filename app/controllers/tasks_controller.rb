400d1963d767c226c1f0455f7b29d5cf17104e15
suwa <baru.web.production@gmail.com>
2021-07-11 17:55:49 +0900

    flashメッセージを作成する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,15 +7,18 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
+      flash[:notice] = 'タスクを作成しました'
       redirect_to root_url
     else
-      render root_path
+      @tasks = Task.all.order(created_at: 'DESC')
+      render :index
     end
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+    redirect_to root_url
   end
 
   private
