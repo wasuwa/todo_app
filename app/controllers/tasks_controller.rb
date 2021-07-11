7842b0fd791f9bf4bc282e4039fa4aded08e99ca
suwa <baru.web.production@gmail.com>
2021-07-12 08:30:27 +0900

    flashメッセージを動的にする

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:notice] = 'タスクを作成しました'
+      flash[:success] = 'タスクを作成しました'
       redirect_to root_url
     else
       @tasks = Task.all.order(created_at: 'DESC')
