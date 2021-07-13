de49dfcc001867b9da0f419dbcd92e8a80b15201
suwa <baru.web.production@gmail.com>
2021-07-14 08:40:09 +0900

    入力された時間が過去だった場合、エラーを発生させる

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,11 @@ class Task < ApplicationRecord
     validates :description, length: { maximum: 255 }
     validates :goal_at
   end
+  validate :not_past_dates
+
+  private
+
+    def not_past_dates
+      errors.add(:goal_at, "過去の日時は指定できません") if goal_at < Time.current
+    end
 end
